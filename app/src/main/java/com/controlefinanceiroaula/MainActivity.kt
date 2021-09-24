ed9b87421d5121b39c0f1b5826eee0a60cfefb27
Luiz Fernando Alves Souza <luizfernandoalvesti@gmail.com>
2021-09-23 22:49:49 -0300

    first action button

--- app/src/main/java/com/controlefinanceiroaula/MainActivity.kt
+++ app/src/main/java/com/controlefinanceiroaula/MainActivity.kt
@@ -2,10 +2,32 @@ package com.controlefinanceiroaula
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Our code here
+        var cost_of_service: EditText = findViewById(R.id.cost_of_service)
+
+        var value_result: TextView = findViewById(R.id.result_view)
+
+        var calculate_button: Button = findViewById(R.id.calculate_button)
+        calculate_button.setOnClickListener {
+
+            var result = calculeValue( cost_of_service.text.toString().toDouble() )
+            value_result.text = result.toString()
+
+        }
     }
+
+    // This method calculate value service
+    fun calculeValue( value_base: Double ): Double {
+        return value_base
+    }
+
 }
