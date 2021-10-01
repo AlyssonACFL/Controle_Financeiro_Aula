b61d6d0ca9d0006fd52ed2aca9b2eba8424fec31
Luiz Fernando Alves Souza <luizfernandoalvesti@gmail.com>
2021-09-30 22:32:31 -0300

    Menu e items

--- app/src/main/java/com/controlefinanceiroaula/MainActivity.kt
+++ app/src/main/java/com/controlefinanceiroaula/MainActivity.kt
@@ -14,43 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Our code here
-        var cost_of_service: EditText = findViewById(R.id.cost_of_service)
 
-        var value_result: TextView = findViewById(R.id.result_view)
-
-        var calculate_button: Button = findViewById(R.id.calculate_button)
-        calculate_button.setOnClickListener {
-
-            if ( TextUtils.isEmpty(cost_of_service.text) ){
-
-                cost_of_service.setError("Informe o Valor")
-
-            }else {
-
-                var result = calculeValue(cost_of_service.text.toString().toDouble())
-                value_result.text = result.toString()
-
-                value_result.visibility = View.VISIBLE
-
-            }
-        }
     }
-
-    // This method calculate value service
-    fun calculeValue( value_base: Double ): Double {
-
-        val option: RadioGroup = findViewById(R.id.tip_options)
-        val selectOption = option.checkedRadioButtonId
-
-        if ( selectOption == R.id.option_twenty_percent )
-            return (value_base * 20.0) / 100 + value_base
-        else if ( selectOption.equals(R.id.option_ten_percent) )
-            return (value_base * 10.0) / 100 + value_base
-        else
-            return (value_base * 5.0) / 100 + value_base
-
-        return value_base
-    }
-
 }
