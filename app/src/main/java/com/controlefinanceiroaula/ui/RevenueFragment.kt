27e92824e575c9c9a87a702bab0b410c5a56843d
AlyssonACFL <63248498+AlyssonACFL@users.noreply.github.com>
2021-10-01 21:37:56 -0300

    saving additions

---
+++ app/src/main/java/com/controlefinanceiroaula/ui/RevenueFragment.kt
@@ -0,0 +1,24 @@
+package com.controlefinanceiroaula.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.controlefinanceiroaula.databinding.FragmentRevenueBinding
+
+class RevenueFragment : Fragment(){
+
+    private var _binding: FragmentRevenueBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+
+    ): View? {
+
+        val root: View = binding.root
+        return root
+
+    }
+
+}
