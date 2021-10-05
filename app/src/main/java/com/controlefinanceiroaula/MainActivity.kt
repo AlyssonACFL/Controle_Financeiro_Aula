9500d044646a594b38236a218c7f45560f060fb5
Luiz Fernando Alves Souza <luizfernandoalvesti@gmail.com>
2021-10-04 21:14:38 -0300

    Navbar

--- app/src/main/java/com/controlefinanceiroaula/MainActivity.kt
+++ app/src/main/java/com/controlefinanceiroaula/MainActivity.kt
@@ -2,17 +2,36 @@ package com.controlefinanceiroaula
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioGroup
-import android.widget.TextView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.controlefinanceiroaula.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        val navView: BottomNavigationView = binding.navigationView
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_extract, R.id.navigation_revenue, R.id.navigation_expense
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
 
     }
